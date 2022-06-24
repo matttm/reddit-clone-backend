0d60571a2ccbf96c8877c1cde87a6a482589e616
Matthew Maloney <matttmaloney@gmail.com>
2022-06-24 19:25:42 -0400

    fixing db

--- server.go
+++ server.go
@@ -23,9 +23,7 @@ func main() {
 		port = defaultPort
 	}
 	database.InitDB()
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
-		DB: db,
-	}}))
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
