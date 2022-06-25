f7ac96e44dcf5a74acd0c2db48b17e1eb8112a5d
Matthew Maloney <matttmaloney@gmail.com>
2022-06-25 17:53:13 -0400

    writing persons query

--- internal/persons/persons.go
+++ internal/persons/persons.go
@@ -1,5 +1,10 @@
 package persons
 
+import (
+	"log"
+	database "reddit-clone-backend/internal/pkg/db/mysql"
+)
+
 type Person struct {
 	Id        string
 	Username  string
@@ -7,4 +12,51 @@ type Person struct {
 	CreatedAt string
 	UpdatedAt string
 	// posts     []*posts.Post
+}
+
+//#2
+func (person Person) Save() int64 {
+	//#3
+	stmt, err := database.Db.Prepare("INSERT INTO PERSONS(USERNAME, PASSWORD) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//#4
+	res, err := stmt.Exec(person.Username, person.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//#5
+	id, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal("Error:", err.Error())
+	}
+	log.Print("Row inserted!")
+	return id
+}
+
+func GetAll() []Person {
+	stmt, err := database.Db.Prepare("SELECT ID, USERNAME FROM PERSONS")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	var persons []Person
+	for rows.Next() {
+		var person Person
+		err := rows.Scan(&person.Id, &person.Username)
+		if err != nil {
+			log.Fatal(err)
+		}
+		persons = append(persons, person)
+	}
+	if err = rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return persons
 }
