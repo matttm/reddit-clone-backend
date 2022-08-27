c7fa2c1820ce4f1513b0a95f9c4615985fea2030
matttm <matttmaloney@gmail.com>
2022-08-27 14:38:41 -0400

    fixing sript

--- scripts/generate.go
+++ scripts/generate.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"strconv"
 
+	"reddit-clone-backend/internal/persons"
 	database "reddit-clone-backend/internal/pkg/db/mysql"
 	"reddit-clone-backend/internal/posts"
-	"reddit-clone-backend/internal/persons"
 
 	_ "github.com/go-sql-driver/mysql"
 
@@ -20,19 +20,21 @@ func main() {
 	defer database.CloseDB()
 	database.Migrate()
 
-	for i := 1; i < 100; i++ {
+	for i := 1; i <= 100; i++ {
 		var person persons.Person
 		person.Username = gofakeit.Username()
 		person.Password = gofakeit.Password(true, true, true, true, false, 10)
 		log.Print("User generated ", person.Username, " ", person.Password)
-		// person.Create()
+		person.Create()
 	}
-	for i := 1; i < 1000; i++ {
+	for i := 1; i < 100; i++ {
 		var post posts.Post
+		var person persons.Person
 		post.Title = gofakeit.Question()
 		post.Body = gofakeit.Paragraph(1, 5, 5, " ")
-		post.Person.Id = strconv.Itoa(gofakeit.Number(1, 100))
-		// log.Print(p"Post generzated ", ost.Title, " ", post.Body)
-		// post.Save()
+		person.Id = strconv.Itoa(gofakeit.Number(1, 100))
+		post.Person = &person
+		log.Print("Post generzated ", post.Title, " ", post.Body, " ", post.Person.Id)
+		post.Save()
 	}
 }
