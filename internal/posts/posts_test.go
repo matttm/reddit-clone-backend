059bca5321f26c910a1458000badc70cb93725b1
matttm <matttmaloney@gmail.com>
2022-10-07 10:29:11 -0400

    adding tests

--- internal/posts/posts_test.go
+++ internal/posts/posts_test.go
@@ -37,6 +37,55 @@ func TestSavePost(t *testing.T) {
 	}
 }
 
+func TestUpdatePost(t *testing.T) {
+	var mock sqlmock.Sqlmock
+	database.Db, mock = NewMock()
+	defer Close()
+
+	var post Post
+	var person persons.Person
+	person.Id = "1"
+	post.Person = &person
+	post.Title = "Test"
+	post.Body = "of the century"
+	post.Views = 0
+
+	query := "UPDATE POSTS SET TITLE = \\?, BODY = \\? WHERE ID = \\?"
+	mock.ExpectPrepare(query)
+	mock.ExpectExec(query).WithArgs(post.Title, post.Body, post.Id).WillReturnResult(sqlmock.NewResult(1, 1));
+	post.Update()
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestDeletePost(t *testing.T) {
+	var mock sqlmock.Sqlmock
+	database.Db, mock = NewMock()
+	defer Close()
+
+	var post Post
+	var person persons.Person
+	person.Id = "1"
+	post.Person = &person
+	post.Title = "Test"
+	post.Body = "of the century"
+	post.Views = 0
+
+	query := "DELETE FROM POSTS WHERE ID = \\?"
+	mock.ExpectPrepare(query)
+	mock.ExpectExec(query).WithArgs(post.Id).WillReturnResult(sqlmock.NewResult(1, 1));
+	post.Delete()
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+
 // TODO: find bwtter placement for this db mock
 func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 	db, mock, err := sqlmock.New()
