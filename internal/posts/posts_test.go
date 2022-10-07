265dd3d7631608d4da72dfbfc2a192072c542433
matttm <matttmaloney@gmail.com>
2022-10-07 19:24:38 -0400

    adding tests

--- internal/posts/posts_test.go
+++ internal/posts/posts_test.go
@@ -85,6 +85,52 @@ func TestDeletePost(t *testing.T) {
 	}
 }
 
+func TestGetAll(t *testing.T) {
+	var mock sqlmock.Sqlmock
+	database.Db, mock = NewMock()
+	defer Close()
+	postMockRows := sqlmock.NewRows([]string{"Id", "Title", "Body", "Viewx"}).
+		AddRow("1", "Genesis Post", "my body", "0").
+		AddRow("2", "Another Post", "my body", "1")
+
+	query := `
+	SELECT post.ID, post.TITLE, post.BODY, post.VIEWS, person.ID,
+		person.USERNAME, person.CREATED_AT, post.CREATED_AT, post.UPDATED_AT FROM POSTS post
+		JOIN PERSONS person ON post.PERSON_ID = person.ID
+	`
+	mock.ExpectPrepare(query)
+	mock.ExpectQuery(query).WillReturnRows(postMockRows)
+	GetAll()
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+func TestGet(t *testing.T) {
+	var mock sqlmock.Sqlmock
+	database.Db, mock = NewMock()
+	defer Close()
+	id := 1
+	postMockRow := sqlmock.NewRows([]string{"Id", "Title", "Body", "Viewx"}).
+		AddRow("1", "Genesis Post", "my body", "0")
+
+	query := `
+	SELECT post.ID, post.TITLE, post.BODY, post.VIEWS, person.ID,
+		person.USERNAME, person.CREATED_AT, post.CREATED_AT, post.UPDATED_AT FROM POSTS post
+		JOIN PERSONS person ON post.PERSON_ID = person.ID WHERE post.ID = \\?
+	`
+	mock.ExpectPrepare(query)
+	mock.ExpectQuery(query).WithArgs(id).WillReturnRows(postMockRow)
+	Get(id)
+
+	// we make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
 
 // TODO: find bwtter placement for this db mock
 func NewMock() (*sql.DB, sqlmock.Sqlmock) {
